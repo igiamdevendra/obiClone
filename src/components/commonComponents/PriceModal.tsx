4d794e564b4794686d6be9dfc41f61580321167d
Devendra Singh <igdevendra83@gmail.com>
2025-04-28 08:42:52 +0530

    updates

--- src/components/commonComponents/PriceModal.tsx
+++ src/components/commonComponents/PriceModal.tsx
@@ -1,20 +1,47 @@
+import { useTranslation } from "react-i18next";
+import {
+  PRODUCTID_ARRIVAL,
+  PRODUCTID_ARRIVALBUNDLE,
+} from "../../constants/commonConstants";
+
 interface PriceModalProps {
-    // title: string;
-    productid: string;
+  // title: string;
+  productid: string;
 }
 
-const PriceModal: React.FC<PriceModalProps> = ({productid}) => {
-    return (
-        <dialog id="my_modal_3" className="modal">
-            <div className="modal-box">
-                <form method="dialog">
-                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-                </form>
-                <h3 className="font-bold text-lg">HI</h3>
-                <p className="py-4">Press ESC key or click on ✕ button to close</p>
-            </div>
-        </dialog>
-    )
-}
+const PriceModal: React.FC<PriceModalProps> = ({ productid }) => {
+  const { t } = useTranslation(["common"]);
+  const getDirectionTranslationKey = () => {
+    let translatoinKey = null;
+    switch (productid) {
+      case PRODUCTID_ARRIVALBUNDLE:
+        translatoinKey = "arrivalDeparture";
+        break;
+      case PRODUCTID_ARRIVAL:
+        translatoinKey = "arrival";
+        break;
+      default:
+        translatoinKey = "departure";
+        break;
+    }
+    return translatoinKey;
+  };
 
-export default PriceModal
+  return (
+    <dialog id="my_modal_3" className="modal">
+      <div className="modal-box">
+        <form method="dialog">
+          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            ✕
+          </button>
+        </form>
+        <h3 className="font-bold text-lg">
+          {t("priceModalHeading", { product: t(getDirectionTranslationKey())})}
+        </h3>
+        <p className="py-4">Press ESC key or click on ✕ button to close</p>
+      </div>
+    </dialog>
+  );
+};
+
+export default PriceModal;
--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,19 +1,22 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
+import * as en from "./en";
+import * as fr from "./fr";
+import * as es from "./es";
+
+const resources = {
+  en,
+  es,
+  fr
+}
+
 async function setupI18n() {
   await i18n.use(initReactI18next).init({
     lng: "en",
     fallbackLng: "en",
     debug: true,
-    resources: {
-      en: {
-        translation: {
-          welcome: "Welcome to my app!",
-          description: "This is an i18next example.",
-        },
-      },
-    },
+    resources,
     interpolation: {
       escapeValue: false,
     },
