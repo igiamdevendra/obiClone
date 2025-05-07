8bce48e82c21b832b9d30b78fc1f5b4d334d924d
devendra singh <igdevendra83@gmail.com>
2025-05-07 18:11:43 +0530

    working on booking form

--- src/components/commonComponents/BookingForm.tsx
+++ src/components/commonComponents/BookingForm.tsx
@@ -1,8 +1,59 @@
 const BookingForm = () => {
+
+  const maxAdult = 5; // You can set this dynamically
+  const options = [];
+
+  for (let i = 0; i <= maxAdult; i++) {
+    options.push(
+      <option key={i} value={i}>
+        {i}
+      </option>
+    );
+  }
+  
   return (
     <>
+      <div className="flex gap-2">
+        <fieldset className="fieldset">
+          <legend className="fieldset-legend">Adult (13+)</legend>
+          <select defaultValue="1" className="select select-sm">
+            {options}
+          </select>
+          <span className="label">Optional</span>
+        </fieldset>
+
+        <fieldset className="fieldset">
+          <legend className="fieldset-legend">Child (2-12) Years</legend>
+          <select defaultValue="Pick a browser" className="select select-sm">
+            <option>0</option>
+            <option>1</option>
+            <option>2</option>
+          </select>
+          <span className="label">Optional</span>
+        </fieldset>
+
+        <fieldset className="fieldset">
+          <legend className="fieldset-legend">Infant (under 2)</legend>
+          <select defaultValue="Pick a browser" className="select select-sm">
+            <option>0</option>
+            <option>1</option>
+            <option>2</option>
+          </select>
+          <span className="label">Optional</span>
+        </fieldset>
+
+      </div>
+
       <div>
-        
+        <fieldset className="fieldset">
+          <legend className="fieldset-legend">Select Your Lounge*</legend>
+          <select defaultValue="select" className="select">
+            <option value="select" disabled={true}>Select</option>
+            <option>Club Mobay</option>
+            <option>Club Kingtson</option>
+          </select>
+          <span className="label">Optional</span>
+        </fieldset>
       </div>
     </>
   )
--- src/components/commonComponents/Loader.tsx
+++ src/components/commonComponents/Loader.tsx
@@ -2,13 +2,15 @@ import { PuffLoader } from "react-spinners";
 
 interface ILoaderProps {
   loading?: boolean;
+  bgClassName: string;
 }
 
-const Loader = ({ loading = true }: ILoaderProps) => {
+const Loader = ({ loading = true}: ILoaderProps) => {
   if (!loading) return null;
+  
 
   return (
-    <div className="fixed top-0 left-0 w-screen h-screen z-[9999] flex items-center justify-center bg-white bg-opacity-50">
+    <div className="fixed top-0 left-0 w-screen h-screen z-[9999] flex items-center justify-center ">
       <PuffLoader />
     </div>
   );
